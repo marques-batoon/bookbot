9f92ac319f99ff4a16e8f1365aa6f00c958df06a
marques-batoon <mbatoon1996@gmail.com>
2026-03-16 17:48:46 -1000

    Created char counter function in stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, char_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +9,6 @@ def main():
     text = get_book_text("/Users/marquesb/BootDev/workspace/Bookbot/books/frankenstein.txt")
     numWords = word_count(text)
     print(f"Found {numWords} total words")
+    print(char_count(text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def char_count(text):
+    char_count = {}
+    for char in text.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
