f15ff9a40d67221c826f9a04e45e4d92012ca538
marques-batoon <mbatoon1996@gmail.com>
2026-03-16 18:02:23 -1000

    Formatted Report

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def get_book_text(filepath):
 
 def main():
     text = get_book_text("/Users/marquesb/BootDev/workspace/Bookbot/books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     numWords = word_count(text)
+    print("----------- Word Count ----------")
     print(f"Found {numWords} total words")
+    print("--------- Character Count -------")
     print(char_count(text))
 
 main()
