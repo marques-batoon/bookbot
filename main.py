2a7cf926ad6cd7d14b7ab7fe522f6735aa7fe3cc
marques-batoon <mbatoon1996@gmail.com>
2026-03-16 17:21:05 -1000

    Created stats.py to contain functions for analyzing text

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
-
-def word_count(text):
-    words = text.split()
-    return len(words)
 
 def main():
     text = get_book_text("/Users/marquesb/BootDev/workspace/Bookbot/books/frankenstein.txt")
